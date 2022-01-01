3f357d07547d07b01d4408ec2b1cd43655bca4dc
ksdhk <ksdhk@naver.com>
2022-01-01 23:01:15 +0900

    Update main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,74 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+        primarySwatch : Colors.blue,
       ),
-        home: FirstPage(),
-      routes:{
-        '/first': (context)=>FirstPage(),
-        '/second': (context)=>SecondPage(),
+      home: MyHomePage(),
+    );
       }
-    );
-  }
-}
-
-class Person{
-  String name;
-  int age;
-
-  Person(this.name,this.age);
-}
-
-class FirstPage extends StatelessWidget{
-
-  @override
-  Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title:Text('first'),
-    ),
-    body: RaisedButton(
-      child:Text('다음페이지로'),
-      onPressed:() async{
-        final person = Person('홍길동',20);
-        final result = await Navigator.pushNamed(context,'/second');
-
-        print(result);
-      },
-    ),
-  );
   }
 
-}
+ class MyHomePage extends StatefulWidget {
+   @override
+   _MyHomePageState createState() => _MyHomePageState();
+ }
 
-class SecondPage extends StatelessWidget{
-  final Person person;
-  SecondPage({@required this.person});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:Text('Second'),
-      ),
-      body: RaisedButton(
-        child:Text('이전 페이지로'),
-        onPressed:(){
-          Navigator.pop(context);
-        },
-      ),
-    );
+ class _MyHomePageState extends State<MyHomePage>{
+    @override
+    Widget build(BuildContext context){
+      return Container();
+    }
   }
-
-}
+  }
