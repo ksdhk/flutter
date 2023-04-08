fd6c4a7e483dd33aa9d4f25496bfa8fd1cc0af89
ksdhk <47198182+ksdhk@users.noreply.github.com>
2023-04-09 00:13:33 +0900

    Create Main.dart

---
+++ flutter_app/Main.dart
@@ -0,0 +1,17 @@
+oid main() {
+
+    String name = '코드팩토리';
+
+    int IsInt = 10;
+
+    double isDouble= 2.5;
+
+    bool isTrue = true;
+
+    print(name);
+    print(IsInt);
+    print(isDouble);
+    print(isTrue);
+
+
+}
